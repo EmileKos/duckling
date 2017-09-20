1fd5dd21928533168e8c7198255a4c12121310a4
DESKTOP-JIUC8RN\Emile Koslowski <koslowski.emile@gmail.com>
2017-09-20 16:26:15 +0200

    error fix

--- Duckling/Numeral/NL/Rules.hs
+++ Duckling/Numeral/NL/Rules.hs
@@ -343,7 +343,7 @@ ruleThousands = Rule
     ]
   , prod = \tokens -> case tokens of
       (Token Numeral (NumeralData {TNumeral.value = v1}) : _) 
-        -> integer v1*1000
+        -> multiply v1 1000
       _ -> Nothing
   }
 
